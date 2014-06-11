3f483707db83a8ed079ada2fb7831633dbbe6b37
mauro <mauroc@yahoo.com>
2014-06-10 21:06:06 -0700

    first commit

---
+++ build/CMakeFiles/squiddio_pi.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mauro/OpenCPN/plugins/squiddio_pi/src/NavObjectCollection.cpp" "/home/mauro/OpenCPN/plugins/squiddio_pi/build/CMakeFiles/squiddio_pi.dir/src/NavObjectCollection.cpp.o"
+  "/home/mauro/OpenCPN/plugins/squiddio_pi/src/Poi.cpp" "/home/mauro/OpenCPN/plugins/squiddio_pi/build/CMakeFiles/squiddio_pi.dir/src/Poi.cpp.o"
+  "/home/mauro/OpenCPN/plugins/squiddio_pi/src/PoiMan.cpp" "/home/mauro/OpenCPN/plugins/squiddio_pi/build/CMakeFiles/squiddio_pi.dir/src/PoiMan.cpp.o"
+  "/home/mauro/OpenCPN/plugins/squiddio_pi/src/icons.cpp" "/home/mauro/OpenCPN/plugins/squiddio_pi/build/CMakeFiles/squiddio_pi.dir/src/icons.cpp.o"
+  "/home/mauro/OpenCPN/plugins/squiddio_pi/src/pugixml.cpp" "/home/mauro/OpenCPN/plugins/squiddio_pi/build/CMakeFiles/squiddio_pi.dir/src/pugixml.cpp.o"
+  "/home/mauro/OpenCPN/plugins/squiddio_pi/src/squiddio_pi.cpp" "/home/mauro/OpenCPN/plugins/squiddio_pi/build/CMakeFiles/squiddio_pi.dir/src/squiddio_pi.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_FILE_OFFSET_BITS=64"
+  "_LARGE_FILES"
+  "__WXGTK__"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
