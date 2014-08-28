d095d52208c6339cde8b9c45a25436e083a11b88
Pavel Kalian <pavel@kalian.cz>
2014-08-28 11:49:14 -0500

    Make more GUI string localizable

--- src/squiddio_pi.cpp
+++ src/squiddio_pi.cpp
@@ -432,19 +432,19 @@ void squiddio_pi::OnContextMenuItemCallback(int id) {
 				RenderLayerContentsOnChart(new_layer);
 
 				if (isLayerUpdate){
-					wxMessageBox(_T("Local destinations have been updated"));
+					wxMessageBox(_("Local destinations have been updated"));
 				}
 			} else {
-				wxMessageBox(_T("No destinations available for the region"));
+				wxMessageBox(_("No destinations available for the region"));
 			}
 		} else {
-			wxMessageBox(_T("Server not responding. Check your Internet connection"));
+			wxMessageBox(_("Server not responding. Check your Internet connection"));
 		}
 	} else if (id == m_report_id) {
 		wxString url_path = _T("http://squidd.io/locations/new?lat=");
 		url_path.Append(wxString::Format(wxT("%f"), m_cursor_lat) << _T("&lon=") << wxString::Format(wxT("%f"), m_cursor_lon));
 		if (!IsOnline() || !wxLaunchDefaultBrowser(url_path))
-			wxMessageBox(_T("Could not launch default browser. Check your Internet connection"));
+			wxMessageBox(_("Could not launch default browser. Check your Internet connection"));
 	}
 }
 
@@ -473,7 +473,7 @@ wxString squiddio_pi::DownloadLayer(){
 	}
 	else
 	{
-		wxMessageBox(_T("Squiddio_pi: unable to connect to host"));
+		wxMessageBox(_("Squiddio_pi: unable to connect to host"));
 	}
 	wxDELETE(httpStream);
 	get.Close();
