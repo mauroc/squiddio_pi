83ab7465b86676169fe75e2d572bf64f35a024d0
Rick Gleason <fcgleason@tgp-architects.com>
2019-09-26 08:35:39 -0400

    Version 1.0.10

--- ci/travis-build-debian.sh
+++ ci/travis-build-debian.sh
@@ -9,18 +9,11 @@ sudo apt-get install devscripts equivs
 
 mkdir  build
 cd build
-<<<<<<< HEAD
-=======
 ln -s ../ci/control.${OCPN_TARGET} ../ci/control
->>>>>>> Update to new Travis process
 mk-build-deps ../ci/control
 sudo apt-get install  ./*all.deb  || :
 sudo apt-get --allow-unauthenticated install -f
 
-<<<<<<< HEAD
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-=======
-cmake -DCMAKE_BUILD_TYPE=Release ..
->>>>>>> Update to new Travis process
 make -sj2
 make package
