64b12ed75cce3d040439a5ace41070e2cf8b6b9b
Rick Gleason <fcgleason@tgp-architects.com>
2020-04-13 13:53:22 -0400

    brew upgrade python and ls

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -26,10 +26,10 @@ set -o pipefail
 for pkg in cairo libexif xz libarchive wget cmake; do
     brew list $pkg 2>/dev/null | head -10 || brew install $pkg
 done
+brew upgrade python
 brew ls python3
-#brew upgrade python
 #pip3 install <package>
-pip3 install setuptools cloudsmith-cli
+#pip3 install setuptools cloudsmith-cli
 #/usr/local/opt/python@3.8/bin/pip3 install cloudsmith-cli setuptools
 
 wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
