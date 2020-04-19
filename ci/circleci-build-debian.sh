5c901723e51b9a4f8f4eca2ed7a3b61137fcebbd
Rick Gleason <fcgleason@tgp-architects.com>
2020-04-19 17:07:52 -0400

    fix circleci-build-debian.sh

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -19,11 +19,11 @@ if [ -n "$BUILD_GTK3" ]; then
   sudo update-alternatives --set wx-config /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
 fi
 
-if [ -n "$tag" ]; then
-  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
-else
-  cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ..
-fi
+#if [ -n "$tag" ]; then
+#  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
+#else
+#  cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr/local ..
+#fi
 
 make -j2
 make package
