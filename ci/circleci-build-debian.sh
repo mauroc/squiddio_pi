90c635b7e2337ca781bb38994d05c630603053be
Rick Gleason <fcgleason@tgp-architects.com>
2020-04-19 20:18:54 -0400

    fix typo 51 and small chgs

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -16,8 +16,7 @@ rm -f ./*all.deb
 tag=$(git tag --contains HEAD)
 
 if [ -n "$BUILD_GTK3" ]; then
-  sudo update-alternatives --set wx-config \
-      /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
+  sudo update-alternatives --set wx-config /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
 fi
 
 if [ -n "$tag" ]; then
