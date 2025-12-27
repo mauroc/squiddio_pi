d1a8cfaa15418c110fc7b66ac1fd0fa252bce452
jongough <2803240+jongough@users.noreply.github.com>
2025-12-28 09:07:37 +1100

    Update to FE2 1.0.352.0

--- ci/circleci-build-macos-universal.sh
+++ ci/circleci-build-macos-universal.sh
@@ -72,14 +72,9 @@ if [[ -z "$CI" ]]; then
     exit 0
 fi
 
-# not-reproducible error on first invocation, seemingly tarball-conf-stamp
+# non-reproducible error on first invocation, seemingly tarball-conf-stamp
 # is not created as required.
-# make package || make package
-make
-make install
-make package
-make package
-
+make package || make package
 
 # Create the cached /usr/local archive
 if [ -n "$CI"  ]; then
